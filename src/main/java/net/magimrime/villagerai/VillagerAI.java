fe0c5042981bde0546bb3353a070706955eb4f53
magimrime <magimrimefilms@gmail.com>
2026-04-20 17:50:08 -0700

    tweaked some villager hazard things

--- src/main/java/net/magimrime/villagerai/VillagerAI.java
+++ src/main/java/net/magimrime/villagerai/VillagerAI.java
@@ -1,9 +1,11 @@
 package net.magimrime.villagerai;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.entity.npc.Villager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,27 +19,29 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(VillagerAI.MODID)
-public class VillagerAI
-{
+public class VillagerAI {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "villagerai";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public VillagerAI()
-    {
+    public VillagerAI() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        Pathfinding.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the
+        // config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void Pathfinding(final FMLCommonSetupEvent event) {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -47,12 +51,21 @@ public class VillagerAI
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
     }
 
+    // Apply water hazard malus to villagers when they join the world
+    @SubscribeEvent
+    public void onEntityJoinLevel(EntityJoinLevelEvent event) {
+        if (event.getEntity() instanceof Villager villager) {
+            Pathfinding.applyVillagerWaterHazard(villager);
+        }
+    }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    // You can use EventBusSubscriber to automatically register all static methods
+    // in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
